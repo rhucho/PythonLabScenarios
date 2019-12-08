47937ff90c3e6d8eb67bca3d43b63c1b84d88465
rhucho <45254097+rhucho@users.noreply.github.com>
2019-12-08 16:07:35 +0530

    Debug the syntax/logical errors in this program

--- lab1.py
+++ lab1.py
@@ -1,16 +1,40 @@
-def gen_fib():
-    count = int(input("How many fibonacci numbers would you like to generate? "))
-    i = 1
-    if count == 0:
-        fib = []
-    elif count == 1:
-        fib = [1]
-    elif count == 2:
-        fib = [1,1]
-    elif count > 2:
-        fib = [1,1]
-        while i < (count - 1):
-            fib.append(fib[i] + fib[i-1])
-            i += 1
+#1
+a = 10
+b = 15
 
-    return fib
+c = a+b/2
+print ('The average of a and b is:',c)
+
+#Prints whether a is big or b is big based on their value. 
+if(a>b)
+	print("Variable a's value is bigger than variable b's value")
+else
+	print("Variable b's value is bigger than variable a's value")
+
+#2
+#print the sum of all the numbers in the List 
+List = [1, '4', 10, 3, 87] 
+List.insert(4, 5)     
+print(List,"\n","Sum is: ", sum(List)) 
+
+#3
+#Appends 50 to the list and then prints the length of the list
+def addToList(mylist): 
+    mylist+= [50] 
+  
+numberlist = [10, 20, 30, 40] 
+addToList(numberlist) 
+print length(numberlist)
+
+#4 
+#Prints Hello 5 times
+counter=0
+While counter < 5:
+    print "Hello"
+    counter = counter + 1
+
+#5
+# Computes y and prints it's value
+x = 3
+y = ((x+1)*(x+2)
+print(y)
